c4ae7974526f743404a1e0282e27746f31eb6a54
dragan002 <draganvujic29@gmail.com>
2024-07-10 13:01:05 +0200

    models done

---
+++ app/models/Book.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\models;
+
+class Book extends Product
+{
+    private int $weight;
+    
+    public function __construct(string $sku, string $name, float $price, int $weight )
+    {
+        parent::__construct($sku, $name, $price, 'Book');
+        $this->weight = $weight;
+    }
+
+    public function setWeight(int $weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    public function getAttribute(): string
+    {
+        return 'Weight: ' . $this->weight . ' Kg';
+    }
+}
---
+++ app/models/Furniture.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\models;
+
+class Furniture extends Product 
+{
+    private int $height;
+    private int $width;
+    private int $length;
+
+    public function __construct(string $sku, string $name, float $price, int $height, int $width, int $length )
+    {
+        parent::__construct($sku, $name, $price, 'Furniture');
+    }
+
+    public function setHeight(int $height): void
+    {
+        $this->height = $height;
+    }
+
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    public function setWidth(int $width): void
+    {
+        $this->width = $width;
+    }
+
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    public function setLength(int $length)
+    {
+        $this->length = $length;
+    }
+
+    public function getLenght(): int
+    {
+        return $this->length;
+    }
+
+    public function getAttribute(): string
+    {
+        return "Dimensions: {$this->height}x{$this->width}x{$this->length} cm";
+    }
+}
